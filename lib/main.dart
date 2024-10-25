1f657d76763a2ffd2a0a68741cc5f696782b4148
Abhishek Singh <mukkulsingh786@gmail.com>
2024-10-25 15:31:52 +0530

    completed reading data from firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:scam_alert/presentation/features/home_page/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -9,8 +16,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: ScamAlertHomePage(),
+    return MaterialApp(
+      home: HomePage(),
     );
   }
 }
